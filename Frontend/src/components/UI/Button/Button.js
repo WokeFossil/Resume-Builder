a74c91d44e251c2dc42c0954229eb956c2ad6a9f
WokeFossil <117059223+WokeFossil@users.noreply.github.com>
2025-10-04 22:19:45 +0530

    Update Button.js

--- Frontend/src/components/UI/Button/Button.js
+++ Frontend/src/components/UI/Button/Button.js
@@ -8,7 +8,7 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
-import Button from '@mui/material/Button'; // MUI Button
+import { Button as MUIButton } from '@mui/material';
 
 export const Button = (props) => {
   const dispatch = useDispatch();
@@ -52,12 +52,12 @@ export const Button = (props) => {
             </DialogContent>
 
             <DialogActions>
-              <Button onClick={handleYes} variant="contained" color="primary">
+              <MUIButton onClick={handleYes} variant="contained" color="primary">
                 Yes
-              </Button>
-              <Button onClick={handleNo} variant="outlined" color="secondary">
+              </MUIButton>
+              <MUIButton onClick={handleNo} variant="outlined" color="secondary">
                 No
-              </Button>
+              </MUIButton>
             </DialogActions>
           </Dialog>
         </>
